00f117800dd2ad9163ab3c51faab2f5836f17d2b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-02-06 07:39:48 +0100

    Support anonymizeIp option.

--- test/test_rack-google-analytics.rb
+++ test/test_rack-google-analytics.rb
@@ -10,7 +10,7 @@ class TestRackGoogleAnalytics < Test::Unit::TestCase
         assert_match %r{\_gaq\.push}, last_response.body
         assert_match %r{\'\_setAccount\', \"somebody\"}, last_response.body
         assert_match %r{</script></head>}, last_response.body
-        assert_equal "532", last_response.headers['Content-Length']
+        assert_equal "533", last_response.headers['Content-Length']
       end
 
       should "not add tracker to none html content-type" do
@@ -31,7 +31,7 @@ class TestRackGoogleAnalytics < Test::Unit::TestCase
       should "add multiple domain script" do
         get "/"
         assert_match %r{'_setDomainName', \"mydomain.com\"}, last_response.body
-        assert_equal "579", last_response.headers['Content-Length']
+        assert_equal "580", last_response.headers['Content-Length']
       end
     end
 
@@ -41,6 +41,14 @@ class TestRackGoogleAnalytics < Test::Unit::TestCase
         get "/"
         assert_match %r{'_setDomainName', 'none'}, last_response.body
         assert_match %r{'_setAllowLinker', true}, last_response.body
+      end
+    end
+
+    context "anonymize IP" do
+      setup { mock_app :async => true, :anonymize_ip => true, :tracker => 'get' }
+      should "set anonymizeIp setting" do
+        get "/"
+        assert_match %r{'_gat._anonymizeIp'}, last_response.body
       end
     end
 
